d7dfe67a7d8412d976155c2af6462c6cf313b124
rmalcolmwood <r.malcolm.wood@gmail.com>
2016-09-06 15:18:35 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here is my solution for assignment 2.
 
-## Write a short comment describing this function
+##step 1 - make a function to define a square invertable matrix called 'x'
+##set the value and set the inverse to NULL (m)
 
-makeCacheMatrix <- function(x = matrix()) {
+makecachematrix <- function(x = matrix()) {
+  m = NULL
+  set = function(y) {
+  x <<- y
+  m <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) m <<- inverse 
+  getinv = function() m
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+  }
 
+##step2 Make a function that checks to see if there is an inverse allready calculated for the matrix.
+cachesolve <- function(x=matrix, ...) {
+  m = x$getinv()
+  if (!is.null(m)){
+  message("getting cached data")
+  return(m)
+  }
+
+##Or else, just calculate it if it doesn't already exist       
+  mat.data = x$get()
+  m = solve(mat.data, ...)
+  x$setinv(m)
+  return(m)
 }
 
+##step 3 assign a matrix to the function to test it
+##run it twice to get the "getting cached data message"
+g<-makecachematrix()
+g$set(matrix(1:4,2,2))
+cachesolve(g)
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cachesolve(g)
+getting cached data
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
